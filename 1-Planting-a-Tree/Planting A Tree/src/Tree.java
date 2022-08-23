6a83930c9550a6a639b18183a9dbba4406e4dd0d
DavidChandlerR <96838080+DavidChandlerR@users.noreply.github.com>
2022-08-23 11:09:17 -0400

    Add Plant A Tree Project

---
+++ 1-Planting-a-Tree/Planting A Tree/src/Tree.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file!
+public class Tree{
+
+    public static void main(String[] args){
+
+// This program will intoduce the programmer and print trees!
+
+        System.out.println("Hey there, I'm David and I'm learning to code in java!");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+    }
+}
